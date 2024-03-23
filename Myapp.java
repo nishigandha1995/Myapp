42d0af13bdba026e0a14d81a231930dd70a8a121
nishigandha1995 <119771780+nishigandha1995@users.noreply.github.com>
2024-03-23 09:05:24 +0530

    Create Myapp.java

---
+++ Myapp.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Myapp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
